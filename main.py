cf07ed2a080bffc2633c8c21dc2f30e9d1e8877e
Marcus Nitzschke <marcus.nitzschke@gmx.com>
2012-10-31 12:09:05 +0000

    [Add] directory structure and main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import sys, os
+package_dir = "packages"
+package_dir_path = os.path.join(os.path.dirname(__file__), package_dir)
+sys.path.insert(0, package_dir_path)
+
+from bottle import run, route
+from movies import views
+
+run(host="localhost", port=8080, debug=True)
---
+++ movies/views.py
@@ -0,0 +1,5 @@
+from bottle import route
+
+@route('/hello')
+def hello():
+    return "Hello World!"
