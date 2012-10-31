5d3eecd72c2f840c8592b47e3ccfb13a2283286a
Martin Junghanns <martin.junghanns@gmx.net>
2012-10-31 13:09:20 +0100

    [Add] using mongodb via pymongo
    see redmine wiki for mongo install instructions

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ package_dir = "packages"
 package_dir_path = os.path.join(os.path.dirname(__file__), package_dir)
 sys.path.insert(0, package_dir_path)
 
+# open connection to mongodb database wcm12
+from pymongo import Connection
+connection = Connection('localhost', 27017)
+db = connection.wcm12
+
 from bottle import run, route
 from movies import views
 
