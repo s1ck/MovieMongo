f6bc0bdf37dcfdf89ddb1de332a7a341093ed258
Martin Junghanns <martin.junghanns@gmx.net>
2012-10-31 14:25:34 +0100

    [Fix] error message when api key is missing

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ package_dir_path = os.path.join(os.path.dirname(__file__), package_dir)
 sys.path.insert(0, package_dir_path)
 
 from bottle import run, route
-from movies import views
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print "usage: main.py <google_api_key>"
         sys.exit(1)
+
+    from movies import views
 
     # run the webserver
     run(host="localhost", port=8080, debug=True)
