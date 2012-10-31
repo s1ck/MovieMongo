22e3804bd06b01df1611f181c5e7a3d6c248b540
Marcus Nitzschke <marcus.nitzschke@gmx.com>
2012-10-31 13:18:30 +0000

    [Add] Bootstrap integration

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,15 @@
-from bottle import route, TEMPLATE_PATH, jinja2_template as template
+from bottle import static_file, route, TEMPLATE_PATH, jinja2_template as template
 
 TEMPLATE_PATH.append("./movies/templates")
+
+# @route('/media/<filename>')
+# def server_static(filename):
+#     return static_file(filename, root='media')
+@route('/media/:path#.+#', name='static')
+def static(path):
+    return static_file(path, root='media')
 
 @route('/')
 def index():
     return template("index.html")
+
