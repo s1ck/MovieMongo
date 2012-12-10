525c090438f0114730dc2937b9fa9e9b63f49cf4
Marcus Nitzschke <marcus.nitzschke@gmx.com>
2012-12-10 15:34:02 +0100

    [Add] added prototypic detail page and the API methods.

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
 import sys
 
-from bottle import static_file, route, TEMPLATE_PATH, jinja2_template as template
+from bottle import static_file, request, route, TEMPLATE_PATH, jinja2_template as template
+from movies.utils.freebase import FreebaseWrapper
 
 TEMPLATE_PATH.append("./movies/templates")
 
@@ -9,22 +10,53 @@ from pymongo import Connection
 connection = Connection('localhost', 27017)
 db = connection.wcm12
 
-# connect to freebase
-from movies.utils.freebase import FreebaseWrapper
-freebase = FreebaseWrapper()
-
-# @route('/media/<filename>')
-# def server_static(filename):
-#     return static_file(filename, root='media')
 @route('/media/:path#.+#', name='static')
 def static(path):
+    """
+    Method for serving static files like stylesheets or images.
+    """
     return static_file(path, root='media')
 
 @route('/')
 def index():
-    return template("index.html")
+    """
+    This methods provides the main page which shows the movie collection of the
+    user or the results of a search.
+    """
+    if "search" in request.params:
+        freebase = FreebaseWrapper()
+        films = freebase.get_film_by_name(request.params["search"])
+        return films
+    else:
+        if request.headers['accept'] == "application/json":
+            # TODO get user movies
+            pass
+        else:
+            return template("index.html")
 
-@route('/film/<film_name>')
-def get_film(film_name):
-    films = freebase.get_film_by_name(film_name)
-    return films
+@route('/:id', method="GET")
+def get_movie(id):
+    """
+    This method delivers all the details available for a given movie.
+    """
+    if request.headers['accept'] == "application/json":
+        freebase = FreebaseWrapper()
+        films = freebase.get_film_by_name(id)
+        return films
+    else:
+        return template("details.html")
+
+@route('/:id', method="PUT")
+def put_movie(id):
+    """
+    This method stores a new movie in the database.
+    """
+    pass
+
+@route('/:id', method="DELETE")
+def delete_movie(id):
+    """
+    This method deletes a given movie either from the users movie collection or
+    completely from the database if no other user owns this movie.
+    """
+    pass
