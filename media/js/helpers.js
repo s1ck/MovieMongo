8643bb0938fb07ff47ff914917202f34ed01d099
Marcus Nitzschke <marcus.nitzschke@gmx.com>
2013-01-23 22:44:23 +0100

    some love for the details page + mockup of user content

--- media/js/helpers.js
+++ media/js/helpers.js
@@ -26,8 +26,7 @@ function format_result(result){
 
 function format_details(result){
   img_url = result.img_url ? result.img_url : "http://placehold.it/250x150";
-  //status = result.my_movie === true ? "V" : "<a href='#' id='add_movie' onclick=\"add_movie(\'"+result.source+"\',\'"+result.id+"\')\">Add</a>";
-  if (result.my_movie === true)
+  if (result.my_movie)
     status = "<a href='#' id='del_movie' title='Remove movie from collection' onclick=\"return del_movie(event,\'"+result._id.$oid+"\');\">"
     +   "<i class='icon-check del'></i>"
     + "</a>";
@@ -36,20 +35,26 @@ function format_details(result){
     +   "<i class='icon-check add'></i>"
     + "</a>";
   
-  out = "<div class='media-body thumbnail'>"
-    + "  <a class='pull-left' href='/"+result.source+"?id="+result.id+"'>"
-    + "    <img class='media-object result' src='" + img_url + "' /> "
-    + "  </a>"
+  out = "<div class='media-body thumbnail' style='padding:10px'>"
+    + "  <img class='media-object result pull-left' style='margin-right:10px' src='" + img_url + "' /> "
     + "  <h3 id='name' class='media-heading'>"+result.name+"</h3> "
     + status
-    + "  <dl class='dl-horizontal' style='margin-top:50px'>"
-    + "  <dt>Source:</dt><dd>" + result.source + "</dd>";
-  out += result.actors.length > 0 ? "<dt>actors:</dt><dd>" + result.actors + "</dd>" : "";
-  out += result.directed_by.length > 0 ? "<dt>directed_by:</dt><dd>" + result.directed_by + "</dd>" : "";
-  out += result.genre.length > 0 ? "<dt>genre:</dt><dd>" + result.genre + "</dd>" : "";
+    + "<p><strong>Source:</strong> " + result.source + "</p>"
+
+    + "  <hr/><div class='pull-left'><h4>Details:</h4><dl class='dl-horizontal'>";
+  out += result.actors.length > 0 ? "<dt>actors:</dt><dd><ul><li>" + result.actors.join("</li><li>") + "</li></ul></dd>" : "";
+  out += result.directed_by.length > 0 ? "<dt>directed_by:</dt><dd><ul><li>" + result.directed_by.join("</li><li>") + "</li></ul></dd>" : "";
+  out += result.genre.length > 0 ? "<dt>genre:</dt><dd><ul><li>" + result.genre.join("</li><li>") + "</li></ul></dd>" : "";
   out += result.initial_release_date ? "  <dt>released:</dt><dd>" + result.initial_release_date + "</dd>" : "";
-  out += result.written_by.length > 0 ? "<dt>written by:</dt><dd>" + result.written_by + "</dd>" : "";
+  out += result.written_by.length > 0 ? "<dt>written by:</dt><dd><ul><li>" + result.written_by.join("</li><li>") + "</li></ul></dd>" : "";
   out += "</dl></div>";
+  if (result.my_movie){
+    out += "<div class='pull-right' style='width:40%'><h4>User content:</h4><div id='forms' class='pull-right'>";
+    out += "<table><tr><td><strong>verborgt an:</strong></td><td>Andreas</td><td><i class='icon-minus-sign'></i></td></tr>";
+    out += "<tr><td><input type='text' class='input-small' placeholder='Key'/></td><td><input type='text' placeholder='Value'/></td><td><i class='icon-plus-sign'></i></td></tr></table>";
+    out += "</div></div>";
+  }
+  out += "</div>";
   return out;
 }
 
