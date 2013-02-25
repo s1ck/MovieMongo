4b88c6894e4dddf136ef4050738eb02f3576d5b3
Marcus Nitzschke <marcus.nitzschke@gmx.com>
2013-02-25 16:13:04 +0100

    [Del] removed python gettext files and test view

--- movies/i18n/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import gettext
-
-path = 'movies/i18n/locales'
-domain = 'messages'
-languages = ['en', 'de']
-translation = gettext.translation(domain, path, languages)
-translation.install()
