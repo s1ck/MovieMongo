c8aa9fef3bea6a2a9a95efff8eb0e017a50ae9d6
Marcus Nitzschke <marcus.nitzschke@gmx.com>
2013-02-25 16:02:48 +0100

    [Add] i18n frontend support
    * modified strings to get recognized by i18n
    * added german translations

--- media/js/helpers.js
+++ media/js/helpers.js
@@ -3,11 +3,11 @@ function format_result(result){
   url = result._id.$oid;//source+"?id="+result.id;
   
   if (result.my_movie === true)
-    status = "<a href='#' id='del_movie' title='Remove movie from collection' onclick=\"return del_movie(event,\'"+result._id.$oid+"\');\">"
+    status = "<a href='#' id='del_movie' title='" + $.i18n("Remove movie from collection") + "' onclick=\"return del_movie(event,\'"+result._id.$oid+"\');\">"
     +   "<i class='icon-check del'></i>"
     + "</a>";
   else
-    status = "<a href='#' id='add_movie' title='Add movie to collection' onclick=\"return add_movie(event,\'"+result._id.$oid+"\');\">"
+    status = "<a href='#' id='add_movie' title='" + $.i18n("Add movie to collection") + "' onclick=\"return add_movie(event,\'"+result._id.$oid+"\');\">"
     +   "<i class='icon-check add'></i>"
     + "</a>";
   
@@ -18,7 +18,7 @@ function format_result(result){
     + "  </a>"
     + "  <a href='/"+url+"'><h3 id='name' class='media-heading'>"+result.name+"</h3></a>&nbsp;"
     + status
-    + "  <p>Source: " + result.source + "</p>"
+    + "  <p>" + $.i18n("Source") + ": " + result.source + "</p>"
     + "</div>"
     + "</li>";
 }
@@ -26,11 +26,11 @@ function format_result(result){
 function format_details(result){
   img_url = result.img_url ? result.img_url : "http://img.freebase.com/api/trans/image_thumb/freebase/no_image_png";
   if (result.my_movie)
-    status = "<a href='#' id='del_movie' title='Remove movie from collection' onclick=\"return del_movie(event,\'"+result._id.$oid+"\');\">"
+    status = "<a href='#' id='del_movie' title='" + $.i18n("Remove movie from collection") + "' onclick=\"return del_movie(event,\'"+result._id.$oid+"\');\">"
     +   "<i class='icon-check del'></i>"
     + "</a>";
   else
-    status = "<a href='#' id='add_movie' title='Add movie to collection' onclick=\"return add_movie(event,\'"+result._id.$oid+"\');\">"
+    status = "<a href='#' id='add_movie' title='" + $.i18n("Add movie to collection") + "' onclick=\"return add_movie(event,\'"+result._id.$oid+"\');\">"
     +   "<i class='icon-check add'></i>"
     + "</a>";
   
@@ -38,20 +38,20 @@ function format_details(result){
     + "  <img class='media-object result pull-left' style='margin-right:10px' src='" + img_url + "' /> "
     + "  <h3 id='name' class='media-heading'>"+result.name+"</h3> "
     + status
-    + "<p><strong>Source:</strong> " + result.source;
+    + "<p><strong>"+ $.i18n("Source") + ":</strong> " + result.source;
   
   if (result.links && result.links.length > 0){
-    out += "<br/><strong>other sources: </strong>";
+    out += "<br/><strong>" + $.i18n("other sources") + ": </strong>";
     for (link in result.links)
       out += "<a href='" + result.links[link].oid.$oid + "'>" + result.links[link].target + "</a> ";
   }
   out += "</p>";
   out += "  <hr/><div class='pull-left'><h4>Details:</h4><dl class='dl-horizontal'>";
-  out += result.actors.length > 0 ? "<dt>actors:</dt><dd><ul><li>" + result.actors.join("</li><li>") + "</li></ul></dd>" : "";
-  out += result.directed_by.length > 0 ? "<dt>directed_by:</dt><dd><ul><li>" + result.directed_by.join("</li><li>") + "</li></ul></dd>" : "";
-  out += result.genre.length > 0 ? "<dt>genre:</dt><dd><ul><li>" + result.genre.join("</li><li>") + "</li></ul></dd>" : "";
-  out += result.initial_release_date ? "  <dt>released:</dt><dd>" + result.initial_release_date + "</dd>" : "";
-  out += result.written_by.length > 0 ? "<dt>written by:</dt><dd><ul><li>" + result.written_by.join("</li><li>") + "</li></ul></dd>" : "";
+  out += result.actors.length > 0 ? "<dt>"+ $.i18n("actors") + ":</dt><dd><ul><li>" + result.actors.join("</li><li>") + "</li></ul></dd>" : "";
+  out += result.directed_by.length > 0 ? "<dt>" + $.i18n("directed_by") + ":</dt><dd><ul><li>" + result.directed_by.join("</li><li>") + "</li></ul></dd>" : "";
+  out += result.genre.length > 0 ? "<dt>" + $.i18n("genre") + ":</dt><dd><ul><li>" + result.genre.join("</li><li>") + "</li></ul></dd>" : "";
+  out += result.initial_release_date ? "  <dt>" + $.i18n("released") + ":</dt><dd>" + result.initial_release_date + "</dd>" : "";
+  out += result.written_by.length > 0 ? "<dt>" + $.i18n("written by") + ":</dt><dd><ul><li>" + result.written_by.join("</li><li>") + "</li></ul></dd>" : "";
   out += "</dl></div>";
   if (result.my_movie){
     out += "<span id='usercontent'></span>"
